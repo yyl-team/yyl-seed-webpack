33c03ac69e184cb44f69924bc942601cf0108ba4
jackness <jackness1208@qq.com>
2019-05-19 01:37:14 +0800

    绕过了 vue.common 的问题

--- examples/vue2-ts/src/entry/index/index.ts
+++ examples/vue2-ts/src/entry/index/index.ts
@@ -1,28 +1,10 @@
 import 'yyl-flexlayout';
 import '@babel/polyfill';
-import Vue from 'vue';
-import Router from 'vue-router'
-import { mapActions } from 'vuex';
-import "./index.scss";
 
-import store from '../../vuex/store.js';
-
-// const pageIndex: AsyncComponent = (): any => import(/* webpackChunkName: "pageIndex" */ '~@/page/index.vue')
-import pageIndex from '~@/page/index.vue';
-// const pageSub: AsyncComponent = (): any => import(/* webpackChunkName: "pageSub" */ '~@/page/sub.vue');
-import pageSub from '~@/page/sub.vue';
-
-Vue.use(Router);
-
-const router = new Router();
+import Vue from 'vue'
+import App from './index.vue'
 
 new Vue({
-  store,
-  router,
-  methods: {
-    // ...mapActions(['addDemoLog'])
-  },
-  mounted() {
-    // this.addDemoLog('index.js ready');
-  }
-}).$mount('#app');
+  el: '#app',
+  render: h => h(App)
+});
