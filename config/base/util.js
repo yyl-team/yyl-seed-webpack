7553801f76afe18dd3e7585c39cf28dd18f1032d
jackness1208 <jackness1208@qq.com>
2020-02-29 12:41:35 +0800

    feat: demo 准备

--- config/base/util.js
+++ config/base/util.js
@@ -1,9 +1,4 @@
-const resolveModule = function (str) {
+module.exports.resolveModule = function (str) {
   return require.resolve(str)
 }
-const webpackMerge = require('webpack-merge')
-
-module.exports = {
-  resolveModule,
-  webpackMerge
-}
+module.exports.webpackMerge = require('webpack-merge')
