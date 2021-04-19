fdaee18cfdc6ae64200444524d4cb661242f2fd2
liudaojie <liudaojie@yy.com>
2021-04-20 00:03:37 +0800

    feat: 为了方便调试 补充 bin 逻辑

---
+++ bin/index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const YylCmdLogger = require('yyl-cmd-logger')
+const chalk = require('chalk')
+const util = require('yyl-util')
+const handler = require('./handler')
+
+const logger = new YylCmdLogger({
+  type: {
+    main: {
+      name: 'SEED',
+      color: chalk.bgRed.white,
+      shortName: 'S',
+      shortColor: chalk.red
+    }
+  }
+})
+
+;(() => {
+  const { env, cmds } = util.cmdParse(process.argv)
+  const ctrl = cmds[0]
+  logger.log('main', ['runner'])
+  if (ctrl in handler) {
+    if (env.ctx) {
+      const ctx = env.ctx
+      delete env.ctx
+      handler[ctrl]({ env, ctx, logger })
+    } else {
+      handler[ctrl]({ env, logger })
+    }
+  } else {
+    logger.log('msg', 'warn', [`usage: ${Object.keys(handler).join(',')}`])
+  }
+})()
