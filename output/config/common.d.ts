af768922103d20ab264c585926a868edcde487fa
liudaojie <liudaojie@yy.com>
2021-05-07 19:36:02 +0800

    feat: 更新依赖

--- output/config/common.d.ts
+++
@@ -1,7 +0,0 @@
-import { Env, YylConfig } from 'yyl-config-types';
-import { Configuration } from 'webpack';
-export interface CommonConfigConfigOption {
-    env: Env;
-    yylConfig: YylConfig;
-}
-export declare function commonConfig(option: CommonConfigConfigOption): Configuration;
