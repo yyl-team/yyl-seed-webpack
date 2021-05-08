a04b536e2a5f6a17d907c72f619df56f1d0e2e54
liudaojie <liudaojie@yy.com>
2021-05-08 17:54:19 +0800

    feat: 去掉敏感信息

--- test/mocha/all.git.shenqu_play.test.js
+++ test/mocha/all.git.shenqu_play.test.js
@@ -1,3 +1,3 @@
 const { handleAllGit } = require('../fn/handle.all')
 
-handleAllGit('https://git*yy*com/webs/yylive_ssr/shenqu_play.git'.split('*').join('.'))
+handleAllGit('https://git__yy__com/webs/yylive_ssr/shenqu_play.git'.split('__').join('.'))
--- test/mocha/all.git.usercenter.test.js
+++ test/mocha/all.git.usercenter.test.js
@@ -1,3 +1,3 @@
 const { handleAllGit } = require('../fn/handle.all')
 
-handleAllGit('https://git*yy*com/webs/web_static/usercenter.git'.split('*').join('.'))
+handleAllGit('https://git__yy__com/webs/web_static/usercenter.git'.split('__').join('.'))
