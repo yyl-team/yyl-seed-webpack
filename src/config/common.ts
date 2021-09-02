8c0ab2d35c465aa97b5d09aec1ed6d225f2def3a
liudaojie <liudaojie@yy.com>
2021-09-02 17:49:10 +0800

    fix: 修复 webpack 生成文件后依然会有箭头函数的问题

--- src/config/common.ts
+++ src/config/common.ts
@@ -17,9 +17,6 @@ export function commonConfig(option: CommonConfigConfigOption): Configuration {
         'querystring': require.resolve('querystring-es3'),
         'webpack/hot': path.join(require.resolve('webpack/hot/emitter.js'), '..'),
         'webpack/hot/emitter': require.resolve('webpack/hot/emitter.js'),
-        'webpack/lib/rules/DescriptionDataMatcherRulePlugin': require.resolve(
-          'webpack/lib/rules/DescriptionDataMatcherRulePlugin.js'
-        ),
         'ansi-html': require.resolve('ansi-html'),
         'html-entities': require.resolve('html-entities'),
         'events': require.resolve('events/'),
